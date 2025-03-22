dc545ef7d16c9264c6a11b9d1a71f87d78136d82
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2025-03-22 20:46:20 +0900

    test: AI Code Reviewer 작동 확인 테스트 (#1)

--- src/main/java/dku/capstone/CapstoneApplication.java
+++ src/main/java/dku/capstone/CapstoneApplication.java
@@ -2,12 +2,18 @@ package dku.capstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class CapstoneApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CapstoneApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, World!";
     }
 
 }
