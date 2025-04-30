4a1f32d79d6be42a10fbd04b6eb669a56945d982
Jhzlo <kjh0010703@naver.com>
2025-04-30 15:54:39 +0900

    fix: User 엔티티 import 경로 수정

--- qrust-domain/src/main/java/com/qrust/user/domain/repository/UserRepository.java
+++ qrust-domain/src/main/java/com/qrust/user/domain/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package com.qrust.user.domain.repository;
 
+import com.qrust.user.domain.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
