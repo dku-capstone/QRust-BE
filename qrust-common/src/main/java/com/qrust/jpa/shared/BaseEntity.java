67cc148d36454945cad832417e616d4b041f41bb
Jhzlo <kjh0010703@naver.com>
2025-04-30 15:11:44 +0900

    feat: BaseEntity 정의

---
+++ qrust-common/src/main/java/com/qrust/jpa/shared/BaseEntity.java
@@ -0,0 +1,19 @@
+package com.qrust.jpa.shared;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
