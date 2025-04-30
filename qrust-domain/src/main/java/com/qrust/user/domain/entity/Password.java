3d3baf6730ff61a3a1be76e8b81654a1bc9256de
Jhzlo <kjh0010703@naver.com>
2025-04-30 15:40:35 +0900

    feat: Password entity 생성

---
+++ qrust-domain/src/main/java/com/qrust/user/domain/entity/Password.java
@@ -0,0 +1,32 @@
+package com.qrust.user.domain.entity;
+
+import com.qrust.infrastructure.jpa.shared.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_password")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Password extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "salt", nullable = false)
+    private String salt;
+
+    @Column(name = "password", nullable = false)
+    private String pwd;
+}
