91b0437626d0c816d0394fda7a377c8dcc927288
harvey <alstjr971@naver.com>
2025-04-30 23:55:21 +0900

    chore: 임시 Security 자동 설정 제거

--- qrust-api/src/main/java/com/qrust/QrustApiApplication.java
+++ qrust-api/src/main/java/com/qrust/QrustApiApplication.java
@@ -2,12 +2,14 @@ package com.qrust;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class QrustApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QrustApiApplication.class, args);
     }
+
 }
