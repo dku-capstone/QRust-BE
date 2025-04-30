1fe23b39832db43d54ba6d38f9487864c512a6a1
Jhzlo <kjh0010703@naver.com>
2025-04-30 15:15:39 +0900

    refactor: getter 추가 및 경로 수정

--- qrust-common/src/main/java/com/qrust/infrastructure/jpa/shared/BaseEntity.java
+++ qrust-common/src/main/java/com/qrust/infrastructure/jpa/shared/BaseEntity.java
@@ -1,12 +1,17 @@
-package com.qrust.jpa.shared;
+package com.qrust.infrastructure.jpa.shared;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+@Getter
 @MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class BaseEntity {
 
     @CreationTimestamp
