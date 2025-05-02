e63a9240e0ab7938fb801e35d52f3e945cebcc1c
Jhzlo <kjh0010703@naver.com>
2025-05-02 18:13:43 +0900

    refactor: ApiResponse 형식 수정

--- qrust-api/src/main/java/com/qrust/common/dto/ApiResponse.java
+++ qrust-api/src/main/java/com/qrust/common/dto/ApiResponse.java
@@ -1,37 +1,25 @@
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import lombok.Getter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
 
-@Getter
-public class ApiResponse<T> {
+public record ApiResponse<T>(
+        @JsonIgnore
+        HttpStatus httpStatus,
+        @NotNull boolean success,
+        @Nullable T data,
+        @Nullable ExceptionDto error
+) {
 
-    private final int code;
-    private final String message;
-    private final T data;
-    private final boolean success;
-    private final String timestamp;
-
-    private ApiResponse(int code, String message, T data, boolean success) {
-        this.code = code;
-        this.message = message;
-        this.data = data;
-        this.success = success;
-        this.timestamp = ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toString();
+    public static <T> ApiResponse<T> ok(@Nullable final T data) {
+        return new ApiResponse<>(HttpStatus.OK, true, data, null);
     }
 
-    public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(ApiResponseCode.SUCCESS.getCode(), ApiResponseCode.SUCCESS.getMessage(), data, true);
+    public static <T> ApiResponse<T> created(@Nullable final T data) {
+        return new ApiResponse<>(HttpStatus.CREATED, true, data, null);
     }
 
-    public static <T> ApiResponse<T> success(String message, T data) {
-        return new ApiResponse<>(ApiResponseCode.SUCCESS.getCode(), message, data, true);
-    }
-
-    public static <T> ApiResponse<T> error(ApiResponseCode errorCode) {
-        return new ApiResponse<>(errorCode.getCode(), errorCode.getMessage(), null, false);
-    }
-
-    public static <T> ApiResponse<T> error(ApiResponseCode errorCode, String message) {
-        return new ApiResponse<>(errorCode.getCode(), message, null, false);
+    public static <T> ApiResponse<T> fail(final CustomException e) {
+        return new ApiResponse<>(e.getErrorCode().getHttpStatus(), false, null, ExceptionDto.of(e.getErrorCode()));
     }
 }
