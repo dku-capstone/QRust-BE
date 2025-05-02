1d2f70dc3693afb8cfd3c0eb0092d7d367663f7a
Jhzlo <kjh0010703@naver.com>
2025-05-02 19:11:40 +0900

    fix: swagger 버전 수정 및 적용

--- qrust-common/src/main/java/com/qrust/config/SwaggerConfig.java
+++ qrust-common/src/main/java/com/qrust/config/SwaggerConfig.java
@@ -1,20 +1,20 @@
 package com.qrust.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "QRust API", description = "QRust : API 명세서", version = "v1.0.0"))
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("QRust API")
-                        .description("QRust API 문서")
-                        .version("1.0.0")
-                );
+    public GroupedOpenApi qrustOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("QRust OPEN API")
+                .pathsToMatch("/**")
+                .build();
     }
 }
