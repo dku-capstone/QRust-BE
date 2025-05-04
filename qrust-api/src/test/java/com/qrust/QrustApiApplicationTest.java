c723560266ab9c14a57bc29b170200e9352e4706
Jhzlo <kjh0010703@naver.com>
2025-05-04 20:55:29 +0900

    fix: local application.yml 하드코딩

--- qrust-api/src/test/java/com/qrust/QrustApiApplicationTest.java
+++ qrust-api/src/test/java/com/qrust/QrustApiApplicationTest.java
@@ -2,10 +2,8 @@ package com.qrust;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("local")
 class QrustApiApplicationTest {
 
     @Test
