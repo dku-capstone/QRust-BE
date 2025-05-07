471404a38c5d5edea7cafb745a5007eda96c2a79
Jhzlo <kjh0010703@naver.com>
2025-05-08 01:59:28 +0900

    feat: user errormessage 상수 관리

---
+++ qrust-common/src/main/java/com/qrust/exception/user/ErrorMessages.java
@@ -0,0 +1,8 @@
+package com.qrust.exception.user;
+
+public class ErrorMessages {
+
+    public static final String USER_NOT_FOUND = "사용자를 찾을 수 없습니다.";
+    public static final String EMAIL_ALREADY_EXISTS = "이미 등록된 이메일입니다.";
+    public static final String INVALID_PHONE_NUMBER = "유효하지 않은 전화번호입니다.";
+}
