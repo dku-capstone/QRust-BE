e7da8f5a558bd9b7cedfb9f92bc8a6e7eef7378d
harvey <alstjr971@naver.com>
2025-05-07 12:48:56 +0900

    feat: QR Code 조회 Controller API 및 Swagger

--- qrust-api/src/main/java/com/qrust/qrcode/controller/QrCodeController.java
+++ qrust-api/src/main/java/com/qrust/qrcode/controller/QrCodeController.java
@@ -1,13 +1,22 @@
 package com.qrust.qrcode.controller;
 
 import com.qrust.common.dto.ApiResponse;
+import com.qrust.common.dto.PageResponse;
 import com.qrust.qrcode.application.QrCodeGeneratorService;
+import com.qrust.qrcode.application.QrCodeQueryService;
 import com.qrust.qrcode.controller.swagger.QrCodeControllerSpec;
 import com.qrust.qrcode.dto.QrCodeGenerateRequestDto;
+import com.qrust.qrcode.dto.QrCodeListResponseDto;
+import com.qrust.qrcode.dto.QrCodeResponseDto;
+import com.qrust.qrcode.dto.QrCodeSearchRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class QrCodeController implements QrCodeControllerSpec {
 
     private final QrCodeGeneratorService qrCodeGeneratorService;
+    private final QrCodeQueryService qrCodeQueryService;
 
     //TODO
     // Authenticated User 연동 + userId 전달
@@ -28,5 +38,37 @@ public class QrCodeController implements QrCodeControllerSpec {
     }
 
     //TODO
-    // QR 코드의 isExpired 필드가 true 인 경우 분기 처리
+    // QR 코드의 Status 필드가 expired 인 경우 분기 처리
+
+    // 목록 조회(페이징)
+    @GetMapping("/user/{userId}")
+    public ApiResponse<PageResponse<QrCodeListResponseDto>> getAllQrCodes(
+            @PathVariable(name = "userId") Long userId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        PageResponse<QrCodeListResponseDto> qrCodeList = qrCodeQueryService.getQrCodes(userId, PageRequest.of(page, size));
+
+        return ApiResponse.ok(qrCodeList);
+    }
+
+    // 단건 조회
+    @GetMapping("/{qrCodeId}")
+    public ApiResponse<QrCodeResponseDto> getQrCode(@PathVariable(name = "qrCodeId") Long qrCodeId) {
+        QrCodeResponseDto qrCode = qrCodeQueryService.getQrCode(qrCodeId);
+
+        return ApiResponse.ok(qrCode);
+    }
+
+    //조건 조회
+    @PostMapping("/search")
+    public ApiResponse<PageResponse<QrCodeListResponseDto>> searchQrCode(
+            @RequestBody QrCodeSearchRequestDto dto,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        PageResponse<QrCodeListResponseDto> result = qrCodeQueryService.searchQrCode(
+                dto, PageRequest.of(page, size));
+
+        return ApiResponse.ok(result);
+    }
 }
--- qrust-api/src/main/java/com/qrust/qrcode/controller/swagger/QrCodeControllerSpec.java
+++ qrust-api/src/main/java/com/qrust/qrcode/controller/swagger/QrCodeControllerSpec.java
@@ -1,14 +1,19 @@
 package com.qrust.qrcode.controller.swagger;
 
 import com.qrust.common.dto.ApiResponse;
+import com.qrust.common.dto.PageResponse;
 import com.qrust.qrcode.dto.QrCodeGenerateRequestDto;
+import com.qrust.qrcode.dto.QrCodeListResponseDto;
+import com.qrust.qrcode.dto.QrCodeResponseDto;
+import com.qrust.qrcode.dto.QrCodeSearchRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Tag(name = "QR 코드 생성 API")
+@Tag(name = "QR 코드 API")
 public interface QrCodeControllerSpec {
 
     @Operation(summary = "QR 코드 생성",
@@ -23,12 +28,55 @@ public interface QrCodeControllerSpec {
                     }
             )
     )
-    ApiResponse<?> generateQrCode(@RequestBody QrCodeGenerateRequestDto dto, Long userId);
+    ApiResponse<?> generateQrCode(@RequestBody QrCodeGenerateRequestDto dto,
+            @Parameter(description = "사용자 ID") Long userId);
+
+
+    @Operation(summary = "QR 코드 목록 조회 (사용자별)")
+    ApiResponse<PageResponse<QrCodeListResponseDto>> getAllQrCodes(
+            @Parameter(description = "사용자 ID") Long userId,
+            @Parameter(description = "페이지 번호", example = "0") int page,
+            @Parameter(description = "페이지 크기", example = "10") int size);
+
+
+    @Operation(summary = "QR 코드 단건 조회")
+    ApiResponse<QrCodeResponseDto> getQrCode(
+            @Parameter(description = "QR 코드 ID") Long qrCodeId);
+
+
+
+    @Operation(summary = "QR 코드 조건 검색",
+            requestBody = @RequestBody(
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    examples = {
+                                            @ExampleObject(name = "조건 검색 예시", value = SEARCH_QR_CODE_REQUEST)
+                                    }
+                            )
+                    }
+            )
+    )
+    ApiResponse<PageResponse<QrCodeListResponseDto>> searchQrCode(
+            @RequestBody QrCodeSearchRequestDto dto,
+            @Parameter(description = "페이지 번호", example = "0") int page,
+            @Parameter(description = "페이지 크기", example = "10") int size);
+
 
     String CREATE_QR_CODE_REQUEST = """
             {
               "url": "https://example.com",
               "title": "QR 코드 제목"
+            }
+            """;
+
+    String SEARCH_QR_CODE_REQUEST = """
+            {
+              "title": "검색할 제목",
+              "type": "URL",
+              "status": "ACTIVE",
+              "start": "2024-01-01T00:00:00",
+              "end": "2025-12-31T23:59:59"
             }
             """;
 }
