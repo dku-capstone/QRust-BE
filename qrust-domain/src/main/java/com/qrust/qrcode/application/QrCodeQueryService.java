e1cc40aa17820db24e41b1fd6937b27f4a8edc9d
harvey <alstjr971@naver.com>
2025-05-07 12:48:37 +0900

    feat: QR Code 조회 Repository, Service 레이어 구현

---
+++ qrust-domain/src/main/java/com/qrust/qrcode/application/QrCodeQueryService.java
@@ -0,0 +1,57 @@
+package com.qrust.qrcode.application;
+
+import com.qrust.common.dto.PageResponse;
+import com.qrust.qrcode.domain.entity.QrCode;
+import com.qrust.qrcode.domain.repository.QrCodeQueryRepository;
+import com.qrust.qrcode.domain.repository.QrCodeRepository;
+import com.qrust.qrcode.dto.QrCodeListResponseDto;
+import com.qrust.qrcode.dto.QrCodeResponseDto;
+import com.qrust.qrcode.dto.QrCodeSearchRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class QrCodeQueryService {
+
+    private final QrCodeQueryRepository qrCodeQueryRepository;
+    private final QrCodeRepository qrCodeRepository;
+
+    public PageResponse<QrCodeListResponseDto> getQrCodes(Long userId, Pageable pageable) {
+
+        Page<QrCodeListResponseDto> result = qrCodeRepository.findAllByUserId(userId, pageable)
+                .map(qrCode -> new QrCodeListResponseDto(
+                        qrCode.getId(),
+                        qrCode.getQrCodeImage().getImageUrl(),
+                        qrCode.getQrCodeData().getTitle(),
+                        qrCode.getCreatedAt(),
+                        qrCode.getQrCodeData().getUrl()
+                ));
+        return PageResponse.from(result);
+    }
+
+    public QrCodeResponseDto getQrCode(Long id) {
+
+        //TODO
+        // 커스텀 예외 처리
+        QrCode qrCode = qrCodeRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("QR 코드를 찾을 수 없습니다."));
+
+        return new QrCodeResponseDto(
+                qrCode.getQrCodeImage().getImageUrl(),
+                qrCode.getQrCodeData().getTitle(),
+                qrCode.getCreatedAt(),
+                qrCode.getQrCodeData().getUrl()
+        );
+    }
+
+    public PageResponse<QrCodeListResponseDto> searchQrCode(QrCodeSearchRequestDto dto, Pageable pageable) {
+        Page<QrCodeListResponseDto> result = qrCodeQueryRepository.searchQrCode(dto, pageable);
+
+        return PageResponse.from(result);
+    }
+}
---
+++ qrust-domain/src/main/java/com/qrust/qrcode/domain/repository/QrCodeQueryRepository.java
@@ -0,0 +1,82 @@
+package com.qrust.qrcode.domain.repository;
+
+import com.qrust.qrcode.domain.entity.QQrCode;
+import com.qrust.qrcode.domain.entity.QQrCodeImage;
+import com.qrust.qrcode.dto.QrCodeListResponseDto;
+import com.qrust.qrcode.dto.QrCodeSearchRequestDto;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class QrCodeQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+    private final QQrCode qrCode = QQrCode.qrCode;
+    private final QQrCodeImage qrCodeImage = QQrCodeImage.qrCodeImage;
+
+    public Page<QrCodeListResponseDto> searchQrCode(QrCodeSearchRequestDto dto, Pageable pageable) {
+        BooleanBuilder condition = buildSearchCondition(dto);
+
+        List<QrCodeListResponseDto> results = queryFactory
+                .select(
+                        Projections.constructor(
+                                QrCodeListResponseDto.class,
+                                qrCode.id,
+                                qrCode.qrCodeImage.imageUrl,
+                                qrCode.qrCodeData.title,
+                                qrCode.createdAt,
+                                qrCode.qrCodeData.url
+                        ))
+                .from(qrCode)
+                .leftJoin(qrCode.qrCodeImage, qrCodeImage)
+                .where(condition)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long total = Optional.ofNullable(
+                queryFactory
+                        .select(qrCode.count())
+                        .from(qrCode)
+                        .where(condition)
+                        .fetchOne()
+        ).orElse(0L);
+
+        return new PageImpl<>(results, pageable, total);
+    }
+
+    private BooleanBuilder buildSearchCondition(QrCodeSearchRequestDto dto) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (dto.title() != null && !dto.title().isBlank()) {
+            builder.and(qrCode.qrCodeData.title.containsIgnoreCase(dto.title()));
+        }
+
+        if (dto.start() != null && dto.end() != null) {
+            builder.and(qrCode.createdAt.between(dto.start(), dto.end()));
+        } else if (dto.start() != null) {
+            builder.and(qrCode.createdAt.goe(dto.start()));
+        } else if (dto.end() != null) {
+            builder.and(qrCode.createdAt.loe(dto.end()));
+        }
+
+        if (dto.type() != null) {
+            builder.and(qrCode.qrCodeType.eq(dto.type()));
+        }
+
+        if (dto.status() != null) {
+            builder.and(qrCode.qrCodeStatus.eq(dto.status()));
+        }
+
+        return builder;
+    }
+}
