50c85ea829730ad43f65a5e0c022e57d760e2119
harvey <alstjr971@naver.com>
2025-05-07 15:32:22 +0900

    feat: JPA의 VO 변경 감지를 위한 @EqualsAndHashCode 어노테이션 명시

--- qrust-domain/src/main/java/com/qrust/qrcode/domain/entity/vo/QrCodeData.java
+++ qrust-domain/src/main/java/com/qrust/qrcode/domain/entity/vo/QrCodeData.java
@@ -2,12 +2,14 @@ package com.qrust.qrcode.domain.entity.vo;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class QrCodeData {
 
