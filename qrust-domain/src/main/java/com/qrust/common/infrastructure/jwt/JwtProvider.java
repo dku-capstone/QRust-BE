b43d562db5d3dbe80216655d4b2413180f6f8116
Jhzlo <kjh0010703@naver.com>
2025-05-09 01:07:35 +0900

    feat: generate refresh token

--- qrust-domain/src/main/java/com/qrust/common/infrastructure/jwt/JwtProvider.java
+++ qrust-domain/src/main/java/com/qrust/common/infrastructure/jwt/JwtProvider.java
@@ -5,14 +5,18 @@ import com.qrust.user.domain.entity.vo.UserRole;
 import io.jsonwebtoken.Jwts;
 import java.time.Instant;
 import java.util.Date;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import javax.crypto.SecretKey;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class JwtProvider {
     private final SecretKeyFactory secretKeyFactory;
+    private final StringRedisTemplate redisTemplate;
     private final SecretKey key = secretKeyFactory.createSecretKey();
 
     public String generateAccessToken(Long userId, UserRole role) {
@@ -28,6 +32,15 @@ public class JwtProvider {
                 .compact();
     }
 
-    public String generateRefreshToken(Long userId, UserRole role) {
+    public String generateRefreshToken(Long userId) {
+        String refreshToken = UUID.randomUUID().toString();
+        String key = "RT::" + refreshToken + "-v1";
+
+        long expirationMillis = JwtConfig.REFRESH_TOKEN_EXPIRATION_TIME;
+        long expirationSeconds = TimeUnit.MILLISECONDS.toSeconds(expirationMillis);
+
+        redisTemplate.opsForValue().set(key, userId.toString(), expirationSeconds, TimeUnit.SECONDS);
+
+        return refreshToken;
     }
 }
