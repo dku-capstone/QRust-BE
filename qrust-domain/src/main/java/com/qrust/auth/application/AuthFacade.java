3fd867fc2ff378d58b56f3f85408cbab0c0abeb6
Jhzlo <kjh0010703@naver.com>
2025-05-09 01:31:42 +0900

    feat: signUp facade 로직 작성

--- qrust-domain/src/main/java/com/qrust/auth/application/AuthFacade.java
+++ qrust-domain/src/main/java/com/qrust/auth/application/AuthFacade.java
@@ -3,13 +3,16 @@ package com.qrust.auth.application;
 import static com.qrust.exception.auth.ErrorMessages.EMAIL_ALREADY_EXISTS;
 
 import com.qrust.auth.dto.SignUpRequest;
+import com.qrust.auth.infrastructure.TokenService;
 import com.qrust.common.exception.CustomException;
 import com.qrust.common.exception.error.ErrorCode;
+import com.qrust.common.infrastructure.jwt.JwtProvider;
 import com.qrust.user.domain.entity.Password;
 import com.qrust.user.domain.entity.User;
 import com.qrust.user.domain.service.PasswordService;
 import com.qrust.user.domain.service.UserService;
 import com.qrust.utils.PasswordHasher;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -18,8 +21,11 @@ import org.springframework.stereotype.Component;
 public class AuthFacade {
     private final UserService userService;
     private final PasswordService passwordService;
+    private final JwtProvider jwtProvider;
+    private final TokenService tokenService;
 
-    public void signUp(SignUpRequest request) { //TODO 메서드 분리
+    @Transactional
+    public void signUp(SignUpRequest request) {
         if (userService.existByEmail(request.email())) {
             throw new CustomException(ErrorCode.NOT_FOUND_END_POINT, EMAIL_ALREADY_EXISTS);
         }
@@ -30,5 +36,13 @@ public class AuthFacade {
         String hashedPassword = PasswordHasher.hash(request.password(), salt);
 
         passwordService.save(Password.of(user.getId(), salt, hashedPassword));
+
+        String at = jwtProvider.generateAccessToken(user.getId(), user.getUserRole());
+        String rt = jwtProvider.generateRefreshToken(user.getId(), user.getUserRole());
+
+        String rtKey = tokenService.getRTKey(user.getId());
+
+        // TODO: 회원가입시 로그인까지?
+        tokenService.saveRT(rtKey,rt);
     }
 }
