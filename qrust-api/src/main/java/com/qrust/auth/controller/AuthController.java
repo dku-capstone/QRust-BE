3fb874307605e307bfeeb7c4403dcc5b1a14962b
Jhzlo <kjh0010703@naver.com>
2025-05-09 03:00:28 +0900

    feat: controller 생성

---
+++ qrust-api/src/main/java/com/qrust/auth/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.qrust.auth.controller;
+
+import com.qrust.auth.application.AuthFacade;
+import com.qrust.auth.dto.LoginRequest;
+import com.qrust.auth.dto.SignUpRequest;
+import com.qrust.auth.swagger.AuthControllerSpec;
+import com.qrust.common.dto.ApiResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController implements AuthControllerSpec {
+    private final AuthFacade authFacade;
+
+    @Override
+    public ApiResponse<Boolean> signup(SignUpRequest request) {
+        authFacade.signUp(request);
+        return ApiResponse.ok(true);
+    }
+
+    @Override
+    public ApiResponse<Boolean> login(LoginRequest request, HttpServletResponse response) {
+        authFacade.login(request, response);
+        return ApiResponse.ok(true);
+    }
+}
---
+++ qrust-api/src/main/java/com/qrust/auth/swagger/AuthControllerSpec.java
@@ -0,0 +1,32 @@
+package com.qrust.auth.swagger;
+
+import com.qrust.auth.dto.LoginRequest;
+import com.qrust.auth.dto.SignUpRequest;
+import com.qrust.common.dto.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Tag(name = "QRust AUTH", description = "회원가입/로그인")
+@RequestMapping("/api/v1/auth")
+public interface AuthControllerSpec {
+
+    @Operation(summary = "회원가입", description = "회원가입")
+    @PostMapping("/signup")
+    ApiResponse<Boolean> signup(
+            @Parameter(description = "회원가입 정보")
+            @RequestBody SignUpRequest request
+    );
+
+    @Operation(summary = "로그인", description = "로그인")
+    @PostMapping("/login")
+    ApiResponse<Boolean> login(
+            @Parameter(description = "로그인 정보")
+            @RequestBody LoginRequest request,
+            HttpServletResponse response
+    );
+}
