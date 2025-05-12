dce99e518f6094c152292d7f0e2c14bf99ee86a5
Jhzlo <kjh0010703@naver.com>
2025-05-12 13:33:18 +0900

    test: jwt provider 테스트

---
+++ qrust-domain/src/test/java/com/qrust/common/infrastructure/jwt/JwtProviderTest.java
@@ -0,0 +1,60 @@
+package com.qrust.common.infrastructure.jwt;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+import com.qrust.user.domain.entity.vo.UserRole;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.Jwts;
+import java.util.UUID;
+import javax.crypto.SecretKey;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class JwtProviderTest {
+
+    @Mock
+    private SecretKeyFactory secretKeyFactory;
+
+    @InjectMocks
+    private JwtProvider jwtProvider;
+
+    @Test
+    void accessToken_생성_시_정상적으로_claim_포함() {
+        // given
+        Long userId = 1L;
+        UserRole role = UserRole.USER;
+        SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+        given(secretKeyFactory.createSecretKey()).willReturn(secretKey);
+
+        // when
+        String token = jwtProvider.generateAccessToken(userId, role);
+
+        // then
+        Claims claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        assertEquals(userId.toString(), claims.getSubject());
+        assertEquals(role.name(), claims.get("role"));
+    }
+
+    @Test
+    void refreshToken_생성_시_UUID형식으로_반환() {
+        // when
+        String refreshToken = jwtProvider.generateRefreshToken();
+
+        // then
+        assertDoesNotThrow(() -> UUID.fromString(refreshToken));
+    }
+}
