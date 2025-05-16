0a85037b07077cec8a8504cd933e726371e173c0
Jhzlo <kjh0010703@naver.com>
2025-05-16 22:26:54 +0900

    feat: AiModel verify url service 로직 작성

---
+++ qrust-domain/src/main/java/com/qrust/external/ai/application/dto/AiModelUrlVerifyService.java
@@ -0,0 +1,20 @@
+package com.qrust.external.ai.application.dto;
+
+import com.qrust.external.ai.application.dto.request.AiModelRequest;
+import com.qrust.external.ai.application.dto.response.AiModelResponse;
+import com.qrust.external.ai.infrastructure.AiModelFeignClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AiModelUrlVerifyService {
+
+    private final AiModelFeignClient aiModelFeignClient;
+
+    public int verifyUrl(String url) {
+        AiModelRequest request = new AiModelRequest(url);
+        AiModelResponse response = aiModelFeignClient.verifyUrl(request);
+        return response.result();
+    }
+}
