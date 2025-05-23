75909c22f998059a6442baf7218bc0000be47173
Jhzlo <kjh0010703@naver.com>
2025-05-23 21:38:46 +0900

    feat: reportFacade 로직 작성

---
+++ qrust-domain/src/main/java/com/qrust/report/application/ReportFacade.java
@@ -0,0 +1,23 @@
+package com.qrust.report.application;
+
+import com.qrust.report.domain.entity.ReportUrl;
+import com.qrust.report.domain.service.PhishingReportService;
+import com.qrust.report.domain.service.ReportUrlService;
+import com.qrust.report.dto.PhishingReportRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class ReportFacade {
+    private final PhishingReportService phishingReportService;
+    private final ReportUrlService reportUrlService;
+
+    @Transactional
+    public void upsertReport(PhishingReportRequest request) {
+        ReportUrl reportUrl = reportUrlService.upsert(request.url());
+        reportUrlService.increaseReportCount(request.url());
+        phishingReportService.save(request, reportUrl);
+    }
+}
