64fc2f5f3151aca7c15f84b887fad7069db59a0a
Jhzlo <kjh0010703@naver.com>
2025-05-23 17:38:36 +0900

    feat: findByUrl 메서드 생성

--- qrust-domain/src/main/java/com/qrust/report/domain/repository/ReportUrlRepository.java
+++ qrust-domain/src/main/java/com/qrust/report/domain/repository/ReportUrlRepository.java
@@ -1,7 +1,9 @@
 package com.qrust.report.domain.repository;
 
 import com.qrust.report.domain.entity.ReportUrl;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReportUrlRepository extends JpaRepository<ReportUrl, Long> {
+    Optional<ReportUrl> findByUrl(String url);
 }
