59dff6a3ed7efceec17c5fef35f567494bdea228
Jhzlo <kjh0010703@naver.com>
2025-05-23 23:38:56 +0900

    feat: UserRole 계층 구조 정의

--- qrust-domain/src/main/java/com/qrust/user/domain/entity/vo/UserRole.java
+++ qrust-domain/src/main/java/com/qrust/user/domain/entity/vo/UserRole.java
@@ -1,7 +1,24 @@
 package com.qrust.user.domain.entity.vo;
 
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Set;
+
 public enum UserRole {
     USER,
     BUSINESS,
-    ADMIN
+    ADMIN;
+
+    private static final Map<UserRole, Set<UserRole>> hierarchy = new EnumMap<>(UserRole.class);
+
+    static {
+        hierarchy.put(USER, Set.of(USER));
+        hierarchy.put(BUSINESS, Set.of(USER, BUSINESS));
+        hierarchy.put(ADMIN, Set.of(USER, BUSINESS, ADMIN));
+    }
+
+    public static boolean isRoleIncluded(UserRole having, UserRole required) {
+        return hierarchy.getOrDefault(having, Collections.emptySet()).contains(required);
+    }
 }
