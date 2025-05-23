59b31bd20f9026d8a670d090f8c02d50d21752ad
Jhzlo <kjh0010703@naver.com>
2025-05-24 01:06:02 +0900

    feat: 코드 리뷰 적용

--- qrust-domain/src/main/java/com/qrust/report/domain/entity/ReportUrl.java
+++ qrust-domain/src/main/java/com/qrust/report/domain/entity/ReportUrl.java
@@ -25,7 +25,7 @@ public class ReportUrl extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "url_address", nullable = false)
+    @Column(name = "url_address", nullable = false,  unique = true)
     private String url;
 
     @Column(name = "report_count", nullable = false)
