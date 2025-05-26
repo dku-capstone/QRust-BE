64308d6519f47912c3ac390fe78a72093cc64671
Jhzlo <kjh0010703@naver.com>
2025-05-26 13:25:55 +0900

    feat: ReportUrlController 생성

---
+++ qrust-api/src/main/java/com/qrust/recognize/controller/ReportUrlController.java
@@ -0,0 +1,23 @@
+package com.qrust.recognize.controller;
+
+import com.qrust.dto.ApiResponse;
+import com.qrust.recognize.swagger.ReportUrlControllerSpec;
+import com.qrust.report.application.UrlRecognizeFacade;
+import com.qrust.report.domain.entity.ReportUrl;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/recognize")
+public class ReportUrlController implements ReportUrlControllerSpec {
+    private final UrlRecognizeFacade urlRecognizeFacade;
+
+    @Override
+    public ApiResponse<Boolean> checkUrl(@RequestParam String url) {
+        return ApiResponse.ok(urlRecognizeFacade.isBlacklistUrl(url));
+    }
+}
---
+++ qrust-api/src/main/java/com/qrust/recognize/swagger/ReportUrlControllerSpec.java
@@ -0,0 +1,17 @@
+package com.qrust.recognize.swagger;
+
+import com.qrust.dto.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Tag(name = "QR Recognize Service", description = "안전한 QR 인식")
+public interface ReportUrlControllerSpec {
+    @Operation(summary = "URL 위험 여부 확인", description = "입력된 URL 신고 테이블 기반 탐지 (true: 악성, false: 안전)")
+    @PostMapping("/report")
+    ApiResponse<Boolean> checkUrl(
+            @Parameter(description = "확인할 URL", example = "http://example.com")
+            String url
+    );
+}
