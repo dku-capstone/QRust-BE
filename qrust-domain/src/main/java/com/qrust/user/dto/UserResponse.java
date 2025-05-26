6456524365bb77ff9f1f9f77edd8a01f7909b6c9
Jhzlo <kjh0010703@naver.com>
2025-05-26 17:07:12 +0900

    fix: UserResponse dto 수정

--- qrust-domain/src/main/java/com/qrust/user/dto/UserResponse.java
+++ qrust-domain/src/main/java/com/qrust/user/dto/UserResponse.java
@@ -1,11 +1,13 @@
 package com.qrust.user.dto;
 
+import com.qrust.user.domain.entity.User;
+
 public record UserResponse(
         Long id,
         String email,
-        String name
+        String userName
 ) {
-    public static UserResponse from(com.qrust.user.domain.entity.User user) {
-        return new UserResponse(user.getId(), user.getEmail(), user.getName());
+    public static UserResponse from(User user) {
+        return new UserResponse(user.getId(), user.getEmail(), user.getUserName());
     }
 }
