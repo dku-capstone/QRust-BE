3bbcfa4ce40c23946090a3cf0fd62990c9ce8ea4
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2025-05-30 21:11:30 +0900

    infra: Nginx 와 HTTPS 프로토콜 요청 포워딩 추가 (#77)
    * chore: HTTPS, HTTP 도메인 주소 origin 추가
    
    * docs: swagger 요청 API 경로 https 고정
    
    * docs: swagger 요청 API 경로 https 고정
    
    * docs: swagger 테스트 경로 운영/로컬 추가
    
    * chore: Health Check 추가
    
    * chore: swagger 설정 롤백

--- qrust-domain/src/main/java/com/qrust/common/config/SecurityConfig.java
+++ qrust-domain/src/main/java/com/qrust/common/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:3000", "http://mayfifth99.store", "https://mayfifth99.store"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
