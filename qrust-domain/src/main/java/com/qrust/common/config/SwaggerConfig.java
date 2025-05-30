c7e6163f743b37285668e5e2f2f3f44cc600fe6e
harvey <alstjr971@naver.com>
2025-05-30 13:57:00 +0900

    chore: swagger 설정 롤백

--- qrust-domain/src/main/java/com/qrust/common/config/SwaggerConfig.java
+++ qrust-domain/src/main/java/com/qrust/common/config/SwaggerConfig.java
@@ -1,13 +1,10 @@
 package com.qrust.common.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
+
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -18,21 +15,13 @@ public class SwaggerConfig {
                 .builder()
                 .group("capstone")
                 .pathsToMatch("/**")
-                .build();
-    }
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("QRust API")
-                        .description("QRust API")
-                        .version("1.0.0")
+                .addOpenApiCustomizer(openApi ->
+                        openApi.setInfo(new Info()
+                                .title("qrust api")
+                                .description("QRust API")
+                                .version("1.0.0")
+                        )
                 )
-                .servers(List.of(
-                        new Server().url("https://mayfifth99.store").description("운영 서버"),
-                        new Server().url("http://localhost:8080").description("로컬 서버")
-                ))
-                ;
+                .build();
     }
 }
